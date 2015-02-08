c73290d927db84cbcbf3561d0a05479d1a75e597
Juha Syrjälä <juha.syrjala@gmail.com>
2015-02-08 19:08:09 +0200

    Add basic layout for login page

--- src/login/login.js
+++ src/login/login.js
@@ -2,6 +2,31 @@
 (function() {
   var module = require('./_index');
   module.controller('LoginPageCtrl', function() {
+  });
+
+  /* <login-form></login-form> */
+  module.directive('loginForm', function loginForm() {
+    return {
+      scope: {},
+      restrict: 'E',
+      replace: true,
+      controller: 'LoginFormCtrl as loginForm',
+      templateUrl: 'login/login-form.html',
+      bindToController: true,
+    };
+  });
+  module.controller('LoginFormCtrl', function LoginFormCtrl(Config) {
+    var self = this;
+    self.form = {};
+    self.appTitle = Config.appTitle;
+
+    self.signIn = function (form) {
+      console.log('signIn', form);
+    };
+
+    self.forgotPassword = function() {
+      console.log('forgotPassword');
+    };
 
   });
 })();
