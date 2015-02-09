122ee1839c36b88c4de1f087256e073c3cad9fb5
Juha Syrjälä <juha.syrjala@gmail.com>
2015-02-09 22:58:40 +0200

    Add fastclick

--- src/main/on_run.js
+++ src/main/on_run.js
@@ -9,15 +9,16 @@
     // change page title based on state
     $rootScope.$on('$stateChangeSuccess', function(event, toState) {
       $rootScope.pageTitle = '';
-
       if ( toState.title ) {
         $rootScope.pageTitle += toState.title;
         $rootScope.pageTitle += ' \u2014 ';
       }
-
       $rootScope.pageTitle += Config.appTitle;
     });
 
+    // https://github.com/ftlabs/fastclick
+    var attachFastClick = require('fastclick');
+    attachFastClick(document.body);
   }
 
   module.exports = OnRun;
